d7a67909ba89f3e332c3f3e3c911b33795445f13
Manish <manishjain.gn2@gmail.com>
2018-12-15 08:12:01 +0530

    mongo db connected in db.js

---
+++ db.js
@@ -0,0 +1,11 @@
+// connect mongoose to database
+const mongoose = require ('mongoose');
+mongoose.connect('mongodb://localhost:27017/krishivDB',(err) => {
+    if(!err) 
+     console.log('MongoDB Connection Suceeded');
+else
+   console.log('Error in DB Connection : ' + JSON.stringify(err, undefined, 2));
+});
+
+module.exports = mongoose;
+
